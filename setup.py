de879b17da0e088dc17f61176fb2200444558782
WOODcraft <121882568+SudoR2spr@users.noreply.github.com>
2024-07-21 19:15:13 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 #This is the place to add your URLs and setup timeout ..
-bot_urls = ['https://replit.com/@WoodCraft/InstagramDownloaderBot#index.js', ]
+bot_urls = ['https://72d96fc7-cc0e-4cf7-b8f2-9cf6a6314e92-00-6329s5i1i51j.sisko.replit.dev:3000/', ]
 timeout = 20 
